b5aac65f1510aa86198bf2b1ca2aad3da54a50dc
Christopher Janse van Rensburg <160723611+ST10434337@users.noreply.github.com>
2026-04-22 19:35:11 +0200

    Delete ST10434337_POE directory

--- ST10434337_POE/GLMSMVC/Models/DTOs/FileDownloadResultDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GLMSMVC.Models.DTOs
-{
-    public class FileDownloadResultDTO // DTO
-    {
-        public byte[] FileBytes { get; set; } = Array.Empty<byte>();
-        public string ContentType { get; set; } = "application/pdf";
-        public string FileName { get; set; } = string.Empty;
-    }
-}
--- ST10434337_POE/GLMSMVC/Models/Enums/ServiceLevel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GLMSMVC.Models.Enums
-{
-    public enum ServiceLevel
-    {
-        Standard,
-        Express,
-        Gold
-    }
-}
